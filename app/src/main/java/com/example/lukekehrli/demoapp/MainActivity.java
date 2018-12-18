ea7217d20c1d3b5ad5efc98ef46dcd8ce9aca3ff
Luke <lukehrli@gmail.com>
2018-12-18 16:23:26 -0500

    Commit

---
+++ app/src/main/java/com/example/lukekehrli/demoapp/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.lukekehrli.demoapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    public void clickFunction(View view){
+
+        EditText myTextField = findViewById(R.id.myTextField);
+
+        Log.i("Info", myTextField.getText().toString());
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
